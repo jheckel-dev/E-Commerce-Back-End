1616ce036ddb0af80724807154be2dcf0f5462f9
jheckel <jalen.heckel@icloud.com>
2021-11-18 13:04:33 -0800

    can add categories to category list

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { restore } = require('../../models/Product');
 
 // The `/api/categories` endpoint
 
@@ -27,7 +28,13 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch(err) {
+    restore.status(500).json(err);
+  }
   // create a new category
 });
 
