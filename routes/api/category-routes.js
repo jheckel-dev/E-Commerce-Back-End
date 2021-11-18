c71e1a49067818aba3d1b0ce65ada2f7abf0b98a
jheckel <jalen.heckel@icloud.com>
2021-11-18 11:14:33 -0800

    added updater to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,11 +36,16 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
