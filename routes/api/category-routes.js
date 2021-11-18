41871cf50d19949749b79e5276336acd84fc6278
jheckel <jalen.heckel@icloud.com>
2021-11-18 10:56:36 -0800

    added attributes to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,8 +7,12 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    include
+    include: {
+      model: 'Product',
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
   })
+  
 });
 
 router.get('/:id', (req, res) => {
