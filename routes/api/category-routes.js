7ff01d49b3c47102094f02c34301dd514924fe11
jheckel <jalen.heckel@icloud.com>
2021-11-18 11:16:30 -0800

    added delete to category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,7 +45,11 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
-  
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
 });
 
 module.exports = router;
