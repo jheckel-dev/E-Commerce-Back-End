8953362cfc6792a90e33313a97e61d120b53c4ac
jheckel <jalen.heckel@icloud.com>
2021-11-18 12:54:52 -0800

    can not get by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,15 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
