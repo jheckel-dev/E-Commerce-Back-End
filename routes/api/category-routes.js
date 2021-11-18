773dc7d8761df7efc836e9560e49586183e5c3a6
jheckel <jalen.heckel@icloud.com>
2021-11-18 11:52:20 -0800

    added catch error response to category get all routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,6 +12,17 @@ router.get('/', (req, res) => {
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
+  .then(dbCatData => {
+    if(!dbCatData) {
+      res.status(404).json({message: 'No categories found'});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 
 });
 
