f36100e4227ac12ff43571ea10bc824c96dd4da1
jheckel <jalen.heckel@icloud.com>
2021-11-18 13:44:23 -0800

    delete route for categories works as it should now

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -45,6 +45,7 @@ router.put('/:id', async (req, res) => {
         id: req.params.id
       }});
 
+    
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -53,8 +54,24 @@ router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'no Category found with this id!' });
+      
+    }
+
+   res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
